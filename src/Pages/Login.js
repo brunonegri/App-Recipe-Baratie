b30aaf51ad2b21ce7710256d653ceffc81954be2
Gabriel Scott <gabrielpcreis@gmail.com>
2022-07-14 19:35:27 -0300

    Formulario travando o botão

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,27 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Login() {
-  return (
-    <div>
-      <input
-        type="email"
-        data-testid="email-input"
-        name="email"
-      />
-      <input
-        type="password"
-        data-testid="password-input"
-        name="password"
-      />
-      <button
-        type="button"
-        data-testid="login-submit-btn"
-      >
-        Enter
+	const [userEmail, setEmail] = useState('');
+	const [userPassword, setPassword] = useState('');
+	const [valid, setValid] = useState(false);
+	const CheckUserInfo = () => {
+		userPassword.length > 6 &&
+			userEmail.includes('@') & setValid((valid = !valid));
+	};
 
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<input
+				type="email"
+				data-testid="email-input"
+				name="email"
+				value={email}
+			/>
+			<input
+				type="password"
+				data-testid="password-input"
+				name="password"
+				value={password}
+				onChange={(setPassword({ password }), CheckUserInfo)}
+			/>
+			<button
+				type="button"
+				data-testid="login-submit-btn"
+				disabled={valid}
+				onClick
+			>
+				Enter
+			</button>
+		</div>
+	);
 }
 
 export default Login;
