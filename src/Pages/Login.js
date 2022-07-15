8294e637acf5317285ca050a96311b3b4838ef8d
Sayuri <ksayuri93@live.com>
2022-07-15 16:30:17 -0300

    Corrige parcialmente erro do lint

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,40 +1,39 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function Login() {
-	const [userEmail, setEmail] = useState('');
-	const [userPassword, setPassword] = useState('');
-	const [valid, setValid] = useState(false);
-	const CheckUserInfo = () => {
-		userPassword.length > 6 &&
-			userEmail.includes('@') & setValid((valid = !valid));
-	};
+  const [userEmail/* , setEmail */] = useState('');
+  const [userPassword, setPassword] = useState('');
+  const six = 6;
+  const CheckUserInfo = () => {
+    userPassword.length > six
+      && userEmail.includes('@') && setValid((valid = !valid));
+  };
 
-	return (
-		<div>
-			<input
-				type="email"
-				data-testid="email-input"
-				name="email"
-				value={email}
-			/>
-			<input
-				type="password"
-				data-testid="password-input"
-				name="password"
-				value={password}
-				onChange={(setPassword({ password }), CheckUserInfo)}
-			/>
-			<button
-				type="button"
-				data-testid="login-submit-btn"
-				disabled={valid}
-				onClick
-			>
-				Enter
-			</button>
-		</div>
-	);
+  return (
+    <div>
+      <input
+        type="email"
+        data-testid="email-input"
+        name="email"
+        value={ email }
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        name="password"
+        value={ password }
+        onChange={ (setPassword({ password }), CheckUserInfo) }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ valid }
+        onClick
+      >
+        Enter
+      </button>
+    </div>
+  );
 }
 
 export default Login;
