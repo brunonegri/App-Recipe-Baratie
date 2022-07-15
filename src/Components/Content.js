1a3cc7ad776b65cd5ebd67b4d36b38f06adec24b
Gabriel Scott <gabrielpcreis@gmail.com>
2022-07-15 16:23:05 -0300

    resolver conflitos

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../Pages/Login';
-import Profile from '../Pages/Profile';
 
 function Content() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/profile" component={ Profile } />
     </Switch>
   );
 }
