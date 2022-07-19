b26be4f9df691c880d3bac8c581365cb458008f4
Bruno Negri <b.negri_@hotmail.com>
2022-07-19 16:20:13 -0300

    REfatorando

--- src/0 - Services/API/requestAPI.js
+++ src/0 - Services/API/requestAPI.js
@@ -1,11 +1,9 @@
-export const fetchRecipeInfos = async (type1, type2, type3, thing) => {
-  const API_URL = `https://www.the${type1}db.com/api/json/v1/1/${type2}.php?${type3}=${thing}`;
+const fetchRecipeInfos = async (type1, type2, type3, thing) => {
+  const API_URL = await `https://www.the${type1}db.com/api/json/v1/1/${type2}.php?${type3}=${thing}`;
   const response = await fetch(API_URL);
-  return response.url;
-};
-
-export const fetchSearch = async (url) => {
-  const response = await fetch(url);
   const data = await response.json();
+  // console.log(data);
   return data;
 };
+
+export default fetchRecipeInfos;
