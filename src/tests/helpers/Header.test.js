f6f8c3adc6a16f2d7d70dc8ac4ac16346b73fb88
Sayuri <ksayuri93@live.com>
2022-07-20 17:43:15 -0300

    Adiciona tests resquisito 9

--- src/tests/helpers/Header.test.js
+++ src/tests/helpers/Header.test.js
@@ -28,6 +28,7 @@ describe('Testando o componente Header', () => {
     const profileIcon = screen.getByTestId('profile-top-btn');
     userEvent.click(profileIcon);
     expect(history.location.pathname).toBe('/profile');
+
   });
 
   test('Verifica se a barra de pesquisa aparece ao clicar na lupa.', () => {
@@ -43,5 +44,58 @@ describe('Testando o componente Header', () => {
 
     userEvent.click(searchIcon);
     expect(screen.queryByTestId(inputSearch)).not.toBeInTheDocument();
+  });
+
+  test('Verifica os inputs radio da barra de pesquisa.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/foods');
+
+    const searchIcon = screen.getByTestId('search-top-btn');
+    userEvent.click(searchIcon);
+
+    expect(screen.queryByTestId('search-input')).toBeInTheDocument();
+    expect(screen.getByTestId('ingredient-search-radio')).toBeInTheDocument();
+    expect(screen.getByTestId('name-search-radio')).toBeInTheDocument();
+    expect(screen.getByTestId('first-letter-search-radio')).toBeInTheDocument();
+    expect(screen.getByTestId('exec-search-btn')).toBeInTheDocument();
+
+    userEvent.click(searchIcon);
+    expect(screen.queryByTestId('search-input')).not.toBeInTheDocument();
+  });
+
+  test('Verifica os inputs radio da barra de pesquisa.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const inputEmail = screen.getByTestId('email-input');
+    userEvent.type(inputEmail, 'teste@teste.com')
+    const inputPassword = screen.getByTestId('password-input');
+    userEvent.type(inputPassword, '1234567')
+    const buttonLogin = screen.getByTestId('login-submit-btn');
+    userEvent.click(buttonLogin)
+    const buttonDrink = screen.getByTestId('drinks-bottom-btn')
+    userEvent.click(buttonDrink)
+
+    const test = screen.getByText(/Drinks/)
+    expect(test).toBeInTheDocument()
+
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    userEvent.click(profileIcon);
+    expect(history.location.pathname).toBe('/profile');
+
+    const buttonDone = screen.getByTestId('profile-done-btn')
+    userEvent.click(buttonDone)
+
+    const test2 = screen.getByText(/Done Recipes/)
+    expect(test2).toBeInTheDocument()
+
+    const profileIcon2 = screen.getByTestId('profile-top-btn');
+    userEvent.click(profileIcon2);
+    expect(history.location.pathname).toBe('/profile');
+
+    const buttonFavorite = screen.getByTestId('profile-favorite-btn')
+    userEvent.click(buttonFavorite)
+
+    const test3 = screen.getByText(/Favorite Recipes/)
+    expect(test3).toBeInTheDocument()
   });
 });
