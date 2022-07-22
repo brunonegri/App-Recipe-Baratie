d8a417cb6af9dc8597c5ce4c9dd6c325d74ba3a2
Sayuri <ksayuri93@live.com>
2022-07-22 15:50:13 -0300

    Adiciona testes do requisito 18

--- src/tests/helpers/Footer.test.js
+++ src/tests/helpers/Footer.test.js
@@ -4,7 +4,7 @@ import renderWithRouter from './renderWithRouter';
 import Footer from '../../Components/Footer';
 import userEvent from '@testing-library/user-event';
 
-describe('Testando o componente Header', () => {
+describe('Testando o componente Footer', () => {
   test('Verifica se é redirecionado para a página do perfil.', () => {
     const { history } = renderWithRouter(<Footer />);
     history.push('/drink');
