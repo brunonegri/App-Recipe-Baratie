71db0d5598fc6a3951b6516cc58fb1e21c61e011
Sayuri <ksayuri93@live.com>
2022-07-22 15:39:37 -0300

    Adiciona testes do requisito 18

---
+++ src/tests/helpers/Footer.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import Footer from '../../Components/Footer';
+import userEvent from '@testing-library/user-event';
+
+describe('Testando o componente Header', () => {
+  test('Verifica se é redirecionado para a página do perfil.', () => {
+    const { history } = renderWithRouter(<Footer />);
+    history.push('/drink');
+
+    const footerFoodbtn = screen.getByTestId('food-bottom-btn');
+    userEvent.click(footerFoodbtn);
+    expect(history.location.pathname).toBe('/foods');
+
+  });
+});
